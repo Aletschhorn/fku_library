8c743d48f7353602b1af181b57d0db80b8239d99
Aletschhorn <99950173+Aletschhorn@users.noreply.github.com>
2022-10-12 22:11:57 +0200

    Fixes #2

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -2,15 +2,26 @@
 defined('TYPO3_MODE') or die();
 
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-	'fku_library',
+	'FkuLibrary',
 	'Library',
 	'Library'
 );
 
-// register flexform
-$pluginSignature = $extensionName.'fkulibrary_library'; 
-$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist'][$pluginSignature] = 'select_key'; 
-$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue($pluginSignature, 'FILE:EXT:fku_library/Configuration/FlexForms/flexform.xml'); 
+\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
+	'FkuLibrary',
+	'LastAdded',
+	'Library last added media'
+);
 
-?>
+\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
+	'FkuLibrary',
+	'Admin',
+	'Library Admin'
+);
+
+// register flexform
+foreach (['lastadded', 'admin'] as $plugin) {
+	$pluginSignature = 'fkulibrary_'.$plugin;
+	$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
+	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue($pluginSignature, 'FILE:EXT:fku_library/Configuration/FlexForms/flexform_'.$plugin.'.xml'); 
+}
--- ext_localconf.php
+++ ext_localconf.php
@@ -1,18 +1,27 @@
 <?php
-defined('TYPO3_MODE') or die();
+use FKU\FkuLibrary\Controller\MediaController;
 
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-	'FKU.fku_library',
-	'Library',
-	array(
-		'Media' => 'search, searchAdmin, list, listNew, listTemporary, new, create, edit, update, delete, export, removeNewFlag, test, idCheck',
-		'Category' => 'list',
-		
-	),
-	// non-cacheable actions
-	array(
-		'Media' => 'search, searchAdmin, list, listNew, listTemporary, new, create, update, delete, export, removeNewFlag, test, idCheck',
-		'Category' => '',
-		
-	)
-);
+defined('TYPO3_MODE') || die();
+
+(static function() {
+	\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+		'FkuLibrary',
+		'Library',
+		[MediaController::class => 'search'],
+		[MediaController::class => 'search']
+	);
+
+	\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+		'FkuLibrary',
+		'LastAdded',
+		[MediaController::class => 'listLastAdded'],
+		[MediaController::class => '']
+	);
+
+	\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+		'FkuLibrary',
+		'Admin',
+		[MediaController::class => 'searchAdmin, list, listNew, listMissing, listTemporary, export, new, create, edit, update, delete, removeNewFlag, test, idCheck'],
+		[MediaController::class => 'searchAdmin, list, listNew, listMissing, listTemporary, export, new, create, edit, update, delete, removeNewFlag, test, idCheck']
+	);
+})();
